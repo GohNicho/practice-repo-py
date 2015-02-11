96340a42ac7c863bf1f933f0a99929cc94acc0dc
Nicholas Goh <n.goh@hotmail.com>
2015-02-11 00:34:29 -0500

    Started the initial implementation

---
+++ linked-lists.py
@@ -0,0 +1,13 @@
+__author__ = 'Nicholas Goh'
+
+class LLNode(self):
+
+    def __init__(self, value, next):
+        self.value = value
+        self.next = next
+
+class LinkedList(self):
+
+    def __init__(self, head):
+        self.head = head
+
